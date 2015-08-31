bb3d58198c23f995b7b1425805840cdf59923ad7
Rémi Collin <remi@plateau.io>
2015-08-31 14:13:27 +0200

    Fix watch .html directory

--- gulpfile.js
+++ gulpfile.js
@@ -46,5 +46,5 @@ gulp.task('default', ['css', 'js', 'browser-sync'], function () {
     gulp.watch("src/scss/*.scss", ['css']);
     gulp.watch("src/scss/*/*.scss", ['css']);
     gulp.watch("src/js/*.js", ['js']);
-    gulp.watch("app/*.html", ['bs-reload']);
+    gulp.watch("public/*.html", ['bs-reload']);
 });
